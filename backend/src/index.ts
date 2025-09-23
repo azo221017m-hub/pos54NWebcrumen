4a46dc3e6f122f8ed7c56c7679e7ffdcd0edd1e5
azo221017m-hub <azo221017m@gmail.com>
2025-09-23 15:30:39 -0600

    Primer commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,16 @@
 import express from 'express';
-import cors from 'cors';
+import { probarConexion } from './db.js';
+import authRoutes from './routes/auth.js';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';  // NOTA: .js aunque sea TS
-import { probarConexion } from './db.js';    // NOTA: .js aunque sea TS
 
 dotenv.config();
-
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
-  await probarConexion(); // solo llamar la función async
+  await probarConexion();
 });
