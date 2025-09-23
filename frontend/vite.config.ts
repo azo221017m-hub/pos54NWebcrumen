884a9cb927b18e30953429826fa737ea2a2b8673
azo221017m-hub <azo221017m@gmail.com>
2025-09-23 13:59:17 -0600

    Primer commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    // Opcional, solo si quieres probar la build localmente con `vite preview`
-    port: 4173,
-    host: true, // permite acceder desde cualquier IP
-    open: true, // abre el navegador automáticamente
-    allowedHosts: ['poswebcrumen.onrender.com'] // tu dominio de Render
+    port: parseInt(process.env.PORT) || 4173, // puerto para vite preview
+    host: true                                // permite acceso externo
   }
 });
