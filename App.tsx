ffda2f34ee5d7afdcb70c7be69be17d07b9d818b
azo221017m-hub <azo221017m@gmail.com>
2025-09-25 11:10:59 -0600

    Primer commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 // src/App.tsx
 import React from 'react';
-import TestApi from './frontend/src/components/TestApi'; // ajusta la ruta real
+import PConfigCrumenPosWeb from './frontend/src/components/PConfigCrumenPosWeb'; // ajusta la ruta real
 
 
 function App() {
   return (
     <div>
       <h1>       Mi App POS</h1>
-      <TestApi />
+      <PConfigCrumenPosWeb />
     </div>
   );
 }
